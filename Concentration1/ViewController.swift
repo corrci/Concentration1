4858af57f596bd1b8c2c63dc00e792c43a6c824b
corrci <jj51816888@qq.com>
2018-11-26 00:01:12 +0800

    add extension Int

--- Concentration1/ViewController.swift
+++ Concentration1/ViewController.swift
@@ -63,6 +63,12 @@ class ViewController: UIViewController {
 
 extension Int {
     var arc4random : Int{
-        return Int(arc4random_uniform(UInt32(self)))
+        if self > 0{
+            return Int(arc4random_uniform(UInt32(self)))
+        }else if self < 0{
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        }else{
+            return 0
+        }
     }
 }
