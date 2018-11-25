b7bffefe049a78ecb1f19b360fda8792467a3446
corrci <jj51816888@qq.com>
2018-11-25 23:31:00 +0800

    add access control

--- Concentration1/ViewController.swift
+++ Concentration1/ViewController.swift
@@ -10,23 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var game = Concentration(numberOfPairsCards: numberOfPairsCards)
+    private lazy var game = Concentration(numberOfPairsCards: numberOfPairsCards)
     
     var numberOfPairsCards: Int{
         return (cardButtons.count + 1) / 2
     }
     
-    var flipCount = 0{
+    private(set) var flipCount = 0{
         didSet{
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
     
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel!
     
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private var cardButtons: [UIButton]!
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let carNumber = cardButtons.index(of: sender){
             game.chooseCard(at: carNumber)
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
             print("chosen card was not in cardButtons")
         }
     }
-    func updateViewFromModel(){
+    private func updateViewFromModel(){
         for index in cardButtons.indices{
             let button = cardButtons[index]
             let card = game.cards[index]
@@ -49,11 +49,11 @@ class ViewController: UIViewController {
         }
     }
     
-    var emojiChoices = ["🍐","🌶","🍌","🌽","🍏","🍓","🍇","🍋","🍉"]
+    private var emojiChoices = ["🍐","🌶","🍌","🌽","🍏","🍓","🍇","🍋","🍉"]
     
-    var emoji = [Int:String]()
+    private var emoji = [Int:String]()
     
-    func emoji(for card: Card) -> String{
+    private func emoji(for card: Card) -> String{
         if emoji[card.identifier] == nil, emojiChoices.count > 0{
             let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
             emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
